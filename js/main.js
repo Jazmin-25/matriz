9c78e0b187484dbf0da1432dfcf03c135cd5c88a
Jaz <jaz.islasg@gmail.com>
2023-06-01 08:27:44 -0600

    Ejercicios de matriz, personas formadas en un banco

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let formados = ["Sofía","David","Juan","Sara","Augustin"]
+let index=0;
+console.log("*******Formados*********")
+index=0;
+while (index<formados.length) {
+    console.log(formados[index]);
+    index++;
+}//while
